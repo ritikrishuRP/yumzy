3a0969d1e4bf06ccf554523c9bbba04ff14e4cf8
rishu <rrishu212@gmail.com>
2025-09-17 18:49:08 +0530

    Project Setup & Auth Controllers

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import authRouter from './routes/auth.routes.js';
+
+
+dotenv.config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRouter);
+
+app.listen(port, ()=>{
+    connectDB();
+    console.log(`Server is running on port ${port}`);
+})
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import { FaRegEye } from 'react-icons/fa' 
+import { FaRegEyeSlash } from 'react-icons/fa'
+import { FcGoogle } from 'react-icons/fc'
+import { useNavigate } from 'react-router-dom'
+
+const SignUp = () => {
+    const primaryColor = "#ff4d2d";
+    const hoverColor = "#e64323";
+    const bgColor = "#fff9f6";
+    const borderColor = "#ddd";
+    const [showPassword, setShowPassword] = useState(false);
+    const [role,setRole]=useState("user");
+    const navigate=useNavigate();
+  return (
+    <div className='min-h-screen w-full flex items-center justify-center p-4' style=
+    {{backgroundColor: bgColor}}>
+        <div className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border-[1px]`}
+        style={{
+            border: `1px solid ${borderColor}`
+        }}>
+            <h1 className={`text-3xl font bold mb-2`} style={{color:primaryColor}}>Yumzy</h1>
+            <p className='text-gray-600 mb-8'>Create your account tp get delicious food </p>
+
+            {/* fullName */}
+
+            <div className='mb-4'>
+                <label htmlFor='fullName' className='block text-gray-700 font-medium mb-1'>Full Name</label>
+                <input type='text' className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' 
+                 placeholder='Enter your full Name' style={{border: `1px solid ${borderColor}`}}/>
+            </div>   
+
+            {/* email */}
+
+            <div className='mb-4'>
+                <label htmlFor='email' className='block text-gray-700 font-medium mb-1'>Email</label>
+                <input type='email' className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' 
+                 placeholder='Enter your email' style={{border: `1px solid ${borderColor}`}}/>
+            </div>   
+
+            {/* Mobile */}
+
+            <div className='mb-4'>
+                <label htmlFor='mobile' className='block text-gray-700 font-medium mb-1'>Mobile</label>
+                <input type='text' className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' 
+                 placeholder='Enter your Mobile Number' style={{border: `1px solid ${borderColor}`}}/>
+            </div>  
+
+            {/* Password */}
+
+            <div className='mb-4'>
+                <label htmlFor='password' className='block text-gray-700 font-medium mb-1'>Password</label>
+                <div className='relative'>
+                    <input type={`${showPassword?"text":"password"}`} className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' 
+                     placeholder='Enter your Password' style={{border: `1px solid ${borderColor}`}}/>
+                <button className='absolute right-3 top-[14px] text-gray-500' onClick={()=>setShowPassword(prev=>!prev)}>{!showPassword?<FaRegEye/>:<FaRegEyeSlash/>}</button>
+                </div>
+            </div>  
+
+            {/* role */}
+
+            <div className='mb-4'>
+                <label htmlFor='role' className='block text-gray-700 font-medium mb-1'>Role</label>
+                <div className='flex gap-2'>
+                    {["user","owner","deliveryBoy"].map((r)=>(
+                        <button className='flex-1 border rounded-lg px-3 py-2 text-center font-medium
+                        transition-colors cursor-pointer'
+                        onClick={()=>setRole(r)}
+                         style={
+                                role==r?
+                                {backgroundColor:primaryColor,color:"white"}
+                                :{border: `1px solid ${primaryColor}`,color:primaryColor}
+                        }>{r}</button>
+                    ))}
+                </div>
+            </div>  
+            <button className={`w-full font-semibold py-2 transition duration-200 bg-[#ff4d2d] text-white hover:bg-[#e64323] cursor-pointer rounded-2xl`}>
+                Sign Up
+            </button>
+
+            <button className='w-full mt-4 flex items-center justify-center gap-2 border rounded-lg
+            px-4 py-2 transition duration-200 border-gray-400 hover:bg-gray-100 cursor-pointer'>
+                <FcGoogle size={20}/>
+                <span>Sign up with google</span>
+            </button>
+            <p className='text-center mt-6 cursor-pointer' onClick={()=>navigate("/signin")}>Already have an account? <span className='text-[#ff4d2d]'>Sign In</span></p>
+        </div>
+    
+    </div>
+  )
+}
+
+export default SignUp
